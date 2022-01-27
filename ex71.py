16be29dcb66045be49cb5a24735e992f038a2a03
Gustavo-AB <g.alvarez.b12@gmail.com>
2022-01-27 13:49:09 -0300

    exc71

--- ex71.py
+++ ex71.py
@@ -1,8 +1,72 @@
-indice = 13
-soma  = k = 0
+print("-="*15)
+print(" "*5, "Caixa Eletronico")
+print("-="*15)
 
-while k < indice:
-    k += 1
-    soma += k
+valor = int(input("Digite o valor que deseja sacar: "))
 
-print(soma)
+cedula50 = 0
+cedula20 = 0
+cedula10 = 0
+moeda1 = 0
+
+while True:
+    if valor >= 50:
+        valor -= 50
+        cedula50 += 1
+
+    elif valor >= 20:
+        valor -= 20
+        cedula20 += 1
+
+    elif valor >= 10:
+        valor -= 10
+        cedula10 += 1
+
+    elif valor >= 1:
+        valor -= 1
+        moeda1 += 1
+
+    elif valor == 0:
+        break
+
+    
+
+print(f"{cedula50} nota(s) de RS50")
+print(f"{cedula20} nota(s) de RS20")
+print(f"{cedula10} nota(s) de RS10")
+print(f"{moeda1} moeda(s) de RS1")
+
+
+'''
+cedula50 = 0
+cedula20 = 0
+cedula10 = 0
+moeda1 = 0
+
+while True:
+    if valor >= 50:
+        valor -= 50
+        cedula50 += 1
+
+    elif valor >= 20:
+        valor -= 20
+        cedula20 += 1
+
+    elif valor >= 10:
+        valor -= 10
+        cedula10 += 1
+
+    elif valor >= 1:
+        valor -= 1
+        moeda1 += 1
+
+    elif valor == 0:
+        break
+
+    
+
+print(f"{cedula50} nota(s) de RS50")
+print(f"{cedula20} nota(s) de RS20")
+print(f"{cedula10} nota(s) de RS10")
+print(f"{moeda1} moeda(s) de RS1")
+'''
