b225b217a3268229a1524e238517a89bc0f889b0
Gustavo-AB <g.alvarez.b12@gmail.com>
2022-02-05 15:35:40 -0300

    exc81 concluido

---
+++ ex81.py
@@ -0,0 +1,20 @@
+numeros = list()
+contador = 0
+
+while True:
+    numeros.append(int(input("Digite um valor: ")))
+    contador += 1
+    continuar_ou_nao = str(input("Deseja continuar [S/N]? "))
+
+    if continuar_ou_nao in "Nn":
+        break
+
+print(f"Ao todo foram {contador} numeros digitados!")
+numeros.sort(reverse=True)
+print(f"A lista em ordem decrescente fica assim {numeros}")
+
+if numeros.count(5) >= 1:
+    print("O valor 5 faz parte da lista!")
+      
+else:
+    print("O valor 5 não faz parte da lista!")
