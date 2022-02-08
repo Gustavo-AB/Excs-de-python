5f7800a65b8fc1fe000504ef5c38b5e1082ae31e
Gustavo-AB <g.alvarez.b12@gmail.com>
2022-02-08 15:24:00 -0300

    exc85 concluido

--- ex85.py
+++ ex85.py
@@ -1,14 +1,13 @@
-numeros = list()
+numeros = [[], []]
 
-#for contador in range(0, 6):
+for contador in range(1, 8):
+    numero = int(input(f"Digite o {contador}° número: "))
 
+    if numero % 2 == 0:
+        numeros[0].append(numero)
 
+    else:
+        numeros[1].append(numero)
 
-
-
-
-
-
-
-
-#°
+print(f"Os numeros pares são {sorted(numeros[0])}!")
+print(f"Os numeros impares são {sorted(numeros[1])}!")
