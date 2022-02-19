86a5e80d03d691456651a908886f46098839b63f
Gustavo-AB <g.alvarez.b12@gmail.com>
2022-02-19 00:42:50 -0300

    exc91 concluido

---
+++ ex91.py
@@ -0,0 +1,24 @@
+from random import randint
+from operator import itemgetter
+
+jogos = {
+    'jogador1': randint(1, 6),
+    'jogador2': randint(1, 6),
+    'jogador3': randint(1, 6),
+    'jogador4': randint(1, 6)
+}
+
+for chave, valor in jogos.items():
+    print(f'{chave}: {valor}')
+    
+ranking = sorted(jogos.items(), key=itemgetter(1), reverse=True)
+
+contador = 0
+contador2 = 1
+
+print("-"*40)
+
+for chave, valor in enumerate(ranking):
+    print(f'{chave+1}° lugar: {valor[0]} com {valor[1]}')
+
+
