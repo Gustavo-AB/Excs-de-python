95f5325a15aaddc8629227688487616970eff879
Gustavo-AB <g.alvarez.b12@gmail.com>
2022-02-19 18:39:56 -0300

    exc92 concluido

--- ex92.py
+++ ex92.py
@@ -1,1 +1,23 @@
-print("Hello world!")
+from datetime import datetime
+
+cadastro = dict()
+
+cadastro["nome"] = str(input('Digite o nome: '))
+
+ano_nascimento = int(input("Ano de nascimento: "))
+cadastro["idade"] = datetime.now().year - ano_nascimento
+
+cadastro["ctps"] = int(input('Carteira de trabalho: (0 caso nao possua)'))
+
+ano_nascimento = 2022 - ano_nascimento
+
+if cadastro["ctps"] != 0:
+    cadastro["contratacao"] = int(input("Ano de contratacao: "))
+    cadastro["salario"] = float(input("Salario: "))
+    cadastro["aposentadoria"] = cadastro["idade"] + ((cadastro["contratacao"] + 35) - datetime.now().year)
+
+print("-"*30)
+
+for chave, valor in cadastro.items():
+    print(f' -{chave} tem o valor {valor}!')
+
