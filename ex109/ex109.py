b944e4fd0a64ad37b691ca3916838b4ddcee06d2
Gustavo-AB <g.alvarez.b12@gmail.com>
2022-02-25 17:06:35 -0300

    exc109 e modulos concluidos

--- ex109/ex109.py
+++ ex109/ex109.py
@@ -2,7 +2,7 @@ import moeda
 
 valor = int(input("Digite um preco: "))
 
-print(f'O valor {moeda.moeda(valor)} aumentado em 23% é igual a {moeda.moeda(moeda.aumentar(valor, 23))}')
-print(f'O valor {moeda.moeda(valor)} diminuido em 23% é igual a {moeda.moeda(moeda.diminuir(valor, 23))}')
-print(f'O dobro de {moeda.moeda(valor)} é igual a {moeda.moeda(moeda.dobro(valor))}')
-print(f'A metade de {moeda.moeda(valor)}  é igual a {moeda.moeda(moeda.metade(valor))}')
+print(f'O valor {moeda.moeda(valor)} aumentado em 23% é igual a {moeda.aumentar(valor, 23, True)}')
+print(f'O valor {moeda.moeda(valor)} diminuido em 23% é igual a {moeda.diminuir(valor, 23, True)}')
+print(f'O dobro de {moeda.moeda(valor)} é igual a {moeda.dobro(valor, True)}')
+print(f'A metade de {moeda.moeda(valor)}  é igual a {moeda.metade(valor, True)}')
--- ex109/moeda.py
+++ ex109/moeda.py
@@ -1,22 +1,26 @@
-def aumentar(numero=0, porcentagem=0):
+def aumentar(numero=0, porcentagem=0, formatacao=False):
     valor_aumentado = numero + ((numero * porcentagem) / 100)
 
-    return valor_aumentado
+    return valor_aumentado if not formatacao else moeda(valor_aumentado)
 
-def diminuir(numero=0, porcentagem=0):
+
+def diminuir(numero=0, porcentagem=0, formatacao=False):
     valor_diminuido = numero - ((numero * porcentagem) / 100)
 
-    return valor_diminuido
+    return valor_diminuido if not formatacao else moeda(valor_diminuido)
 
-def dobro(numero=0):
+
+def dobro(numero=0, formatacao=False):
     dobro = numero * 2
 
-    return dobro
+    return dobro if not formatacao else moeda(dobro)
 
-def metade(numero=0):
+
+def metade(numero=0, formatacao=False):
     metade = numero / 2
 
-    return metade
+    return metade if not formatacao else moeda(metade)
+
 
 def moeda(numero=0, moeda='R$'):
     return f'{moeda}{numero:.2f}'.replace('.', ',')
